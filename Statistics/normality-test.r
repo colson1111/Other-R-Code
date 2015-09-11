b972c016f9974b3a2a03cc3764f2fc97883f3422
colson1111 <colson1111@gmail.com>
2015-09-10 20:54:15 -0400

    Create normality-test.r

---
+++ Statistics/normality-test.r
@@ -0,0 +1,33 @@
+
+# http://datascienceplus.com/normality-tests-for-continuous-data/
+install.packages("nortest")
+
+library(nortest)
+
+# random normal distributed data
+x <- rnorm(100, 10, 1)
+
+# random weibull distributed data
+y <- rweibull(100, 1, 5)
+
+
+# ANDERSON-DARLING NORMALITY TEST
+# Null Hypothesis:  The data are normally distributed
+# Alternative Hypohtesis:  The data are not normally distributed
+
+# Note:  susceptible to extreme values.  In these cases, try Shapiro-Wilk Test
+
+ad.test(x)  # the normally distributed x dataset has p-value greater than 0.05
+
+ad.test(y)  # the weibull distributed y dataset has p-value less than 0.05
+
+
+# NORMAL PROBABILITY / QQ PLOT
+qqnorm(x)
+qqline(x, col = "red")  # the normally distributed data fall on the line
+
+qqnorm(y)
+qqline(y, col = "blue") # the non-normal data display significant curvature
+
+
+
